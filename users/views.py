c90e07dde2865ffad5b182ba06274f39f59b3412
AntonIsaksson <anton.isak@outlook.com>
2020-05-26 16:33:20 +0200

    Added if statement for registration to save created user, added messages

--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
+from django.contrib import messages
 
 def registration(request):
-    form = UserRegistrationForm
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created!')
+            return redirect('/')
+
+    else:
+        form = UserRegistrationForm()
     return render(request, 'users/registration.html', {'form': form})
