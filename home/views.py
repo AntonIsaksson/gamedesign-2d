6edee194396b8c266bda8c24fb4e8ebfbc647dcf
AntonIsaksson <anton.isak@outlook.com>
2020-06-07 22:54:24 +0200

    Improved home-view and added image sliders

--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from graphics.models import Creature, Object, Landscape
 
-def home(request):
-    context = {
-        'creatures': Creature.objects.all(),
-        'objects': Object.objects.all(),
-        'landscapes': Landscape.objects.all()
-    }
-    return render(request, 'home/home.html', context)
+# def home(request):
+#     context = {
+#         'creatures': Creature.objects.all(),
+#         'objects': Object.objects.all(),
+#         'landscapes': Landscape.objects.all()
+#     }
+#     return render(request, 'home/home.html', context)
 
+def home(request):
+    creatures = Creature.objects.all()
+    objects = Object.objects.all()
+    landscapes = Landscape.objects.all()
+    return render(request, 'home/home.html', {'creatures': creatures, 'objects': objects, 'landscapes': landscapes})
